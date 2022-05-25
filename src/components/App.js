0fc71bacd554ab74d9ee857a582e4c8a181fee5b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-25 16:57:21 -0700

    Removed code that was added to TicketList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import TicketList from "./TicketList";
+
 
 function App(){
-  const name = "Thato";
-  const name2 = "Haley";
   return (
     <React.Fragment>
       <Header />
-      <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      {/* This is a JSX comment. */}
-      <hr/>
+      <TicketList />
     </React.Fragment>
   );
 }
