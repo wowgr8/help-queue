af71ea1e3750166151645579afb3c69f8580916e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-25 20:01:23 -0700

    add fixed indentation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ticketsImage from "./../img/tickets.png"
+import ticketsImage from "./../img/tickets.png";
 
 function Header(){
-  return(
+  return (
     <React.Fragment>
       <h1>Help Queue</h1>
       <img src={ticketsImage} alt="An image of tickets" />
